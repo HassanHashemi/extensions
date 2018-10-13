a3d721c958d06a890206b4b72f2e8cbafbcef418
Mahdi Fathali <mohammadmahdifathali@gmail.com>
2018-10-13 15:49:21 +0330

    object Extension

--- src/Extensions/ObjectExtensions.cs
+++ src/Extensions/ObjectExtensions.cs
@@ -9,14 +9,14 @@ namespace System
         {
             if (source == null)
             {
-                return false;
+                throw new ArgumentNullException(nameof(source));
             }
             return source.GetType()
                     .GetProperties(BindingFlags.Public | BindingFlags.Instance)
                         .Any(p => p.Name == propertyName);
         }
 
-        public static object GetPropertyVaue(this object source, string name)
+        public static object GetPropertyValue(this object source, string name)
         {
             if (source == null)
             {
@@ -25,19 +25,25 @@ namespace System
             var type = source.GetType();
             var property = type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
                 .FirstOrDefault(p => p.Name == name);
-
+            if (property == null)
+            {
+                throw new ArgumentNullException(nameof(property));
+            }
             return property.GetValue(source);
         }
 
         public static void SetProperty(this object source, string propertyName, object value)
         {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
             var type = source.GetType();
             var property = type.GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance);
             if (property == null)
             {
                 throw new InvalidOperationException($"property {propertyName} not found.");
             }
-
             property.SetValue(source, value);
         }
     }

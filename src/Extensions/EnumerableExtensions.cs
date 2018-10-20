942542a44aa3451f5aae6c1eb5ce386403fc95e7
Hassan Hashemi <HassanHashemi@yahoo.com>
2018-10-20 10:21:35 +0330

    Add 'Contains element method' to collection helpers

--- src/Extensions/EnumerableExtensions.cs
+++ src/Extensions/EnumerableExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace System
 {
@@ -6,12 +7,24 @@ namespace System
     {
         public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action)
         {
-            foreach(var item in source)
+            foreach (var item in source)
             {
                 action(item);
             }
 
             return source;
+        }
+
+        /// <summary>
+        /// returns true if collection contains any element
+        /// and false if collection is null or empty
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static bool ContainsElement<T>(this IEnumerable<T> source)
+        {
+            return source != null && source.Any();
         }
     }
 }

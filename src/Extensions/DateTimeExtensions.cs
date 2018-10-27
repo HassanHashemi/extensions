93010600a94637a3580c90ae0eddd5b4e2c00462
Hassan Hashemi <HassanHashemi@yahoo.com>
2018-10-27 14:10:19 +0330

    Making time nullable

--- src/Extensions/DateTimeExtensions.cs
+++ src/Extensions/DateTimeExtensions.cs
@@ -10,9 +10,9 @@ namespace Project.Base
         public int Year { get; set; }
         public int Month { get; set; }
         public int Day { get; set; }
-        public int Hours { get; set; }
-        public int Minutes { get; set; }
-        public int Seconds { get; set; }
+        public int? Hours { get; set; }
+        public int? Minutes { get; set; }
+        public int? Seconds { get; set; }
 
         public string Full => $"{this.Year}/{this.Month}/{this.Day}";
     }

1b5575b41eb3839f8b526d22cc685cace91b6d40
Hassan Hashemi <HassanHashemi@yahoo.com>
2018-11-04 13:06:07 +0330

    add match helpers

---
+++ src/Extensions/Regex/RegexHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Extensions
+{
+    public static class RegexHelpers
+    {
+        public static bool Match(string input, string pattern)
+        {
+            return Match(input, pattern, TimeSpan.FromMilliseconds(300));
+        }
+
+        public static bool Match(string input, string pattern, TimeSpan timeout)
+        {
+            return Regex.IsMatch(input, pattern, default, timeout);
+        }
+    }
+}

0737e92763e6313208c859e846d7e385b31ff181
Mahdi Fathali <mohammadmahdifathali@gmail.com>
2018-11-17 13:40:39 +0330

    tagExtraction and test

---
+++ src/Extensions/hashtagUtility/hashtagUtility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Extensions.hashtagUtility
+{
+    public class HashtagUtility
+    {
+        public List<string> TagExteraction(string input)
+        {
+            return Regex.Matches(input, @"\b?\#\w*\b")
+                    .Cast<Match>()
+                    .Select(i => i.Value)
+                    .ToList();
+        }
+    }
+}
---
+++ test/ExtensionsTest/HashUtilityTest.cs
@@ -0,0 +1,22 @@
+﻿using Extensions.hashtagUtility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ExtensionsTest
+{
+    public class HashUtilityTest
+    {
+        [Theory]
+        [InlineData("در جریان #قتل #خاشقچی #دولت_عربستان  مقصر شناخته شده است! اما این #دیوث ها زیر بار نمی روند البته این مهم نیست چون #وهابیت=انگلیسی هست!")]
+        public void Should_Pass_TagExteraction(string data)
+        {
+            var hashtag = new HashtagUtility();
+            var tagList = hashtag.TagExteraction(data);
+            Assert.True(tagList[2] == "#دولت_عربستان");
+            Assert.False(tagList[3] == "#دیوث ها");
+            Assert.True(tagList[4] == "#وهابیت");
+        }
+    }
+}

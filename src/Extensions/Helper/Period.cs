9c646b9ac4a20e4578d742d8254aa7e9ab24e9a6
Mohsen <khodaparastan@gmail.com>
2018-11-20 10:44:04 +0330

    https://gitlab.com/TebyanSoftware/Nugets/extensions/merge_requests/13#note_118542479

--- src/Extensions/Helper/Period.cs
+++ src/Extensions/Helper/Period.cs
@@ -32,6 +32,7 @@ namespace Extensions.Helper
 
 			var span = first - second;
 			months = 12 * (first.Year - second.Year) + (first.Month - second.Month);
+			//-500 : برای تاریخ یکسان کاربرد دارد
 			if (first.CompareTo(second.AddMonths(months).AddMilliseconds(-500)) <= 0)
 			{
 				months--;
@@ -66,78 +67,6 @@ namespace Extensions.Helper
 			minutes = span.Minutes;
 
 			return new Period(years, months, week, days, hours, minutes);
-		}
-	}
-
-	internal sealed class PeriodWrapper
-	{
-		internal PeriodWrapper(Period period)
-		{
-			_period = period;
-		}
-
-		public string Result { get; private set; }
-
-		private readonly Period _period;
-
-		public PeriodWrapper WithYear()
-		{
-			if (_period.Years > 0)
-			{
-				Result = $"{_period.Years} سال پیش";
-			}
-
-			return this;
-		}
-
-		public PeriodWrapper WithMonth()
-		{
-			if (_period.Years < 1 && this._period.Months > 0)
-			{
-				Result = $"{_period.Months} ماه پیش";
-			}
-
-			return this;
-		}
-
-		public PeriodWrapper WithWeek()
-		{
-			if (_period.Months < 1 && _period.Weeks > 0)
-			{
-				Result = $"{_period.Weeks} هفته پیش";
-			}
-
-			return this;
-		}
-
-		public PeriodWrapper WithDay()
-		{
-			if (_period.Weeks < 1 && _period.Days > 0)
-			{
-				Result = $"{_period.Days} روز پیش";
-			}
-
-			return this;
-		}
-
-		public PeriodWrapper WithHour()
-		{
-			if (_period.Days < 1 && _period.Hours > 0)
-			{
-				Result = $"{_period.Hours} ساعت پیش";
-			}
-
-			return this;
-		}
-
-		public PeriodWrapper WithMinutes()
-		{
-			if (_period.Hours < 1 && _period.Minutes > 0)
-			{
-				Result = $"{_period.Minutes} دقیقه پیش";
-			}
-
-			return this;
 		}
 	}
 }
---
+++ src/Extensions/Helper/PeriodWrapper.cs
@@ -0,0 +1,74 @@
+﻿namespace Extensions.Helper
+{
+	internal sealed class PeriodWrapper
+	{
+		internal PeriodWrapper(Period period)
+		{
+			_period = period;
+		}
+
+		public string Result { get; private set; }
+
+		private readonly Period _period;
+
+		public PeriodWrapper WithYear()
+		{
+			if (_period.Years > 0)
+			{
+				Result = $"{_period.Years} سال پیش";
+			}
+
+			return this;
+		}
+
+		public PeriodWrapper WithMonth()
+		{
+			if (_period.Years < 1 && this._period.Months > 0)
+			{
+				Result = $"{_period.Months} ماه پیش";
+			}
+
+			return this;
+		}
+
+		public PeriodWrapper WithWeek()
+		{
+			if (_period.Months < 1 && _period.Weeks > 0)
+			{
+				Result = $"{_period.Weeks} هفته پیش";
+			}
+
+			return this;
+		}
+
+		public PeriodWrapper WithDay()
+		{
+			if (_period.Weeks < 1 && _period.Days > 0)
+			{
+				Result = $"{_period.Days} روز پیش";
+			}
+
+			return this;
+		}
+
+		public PeriodWrapper WithHour()
+		{
+			if (_period.Days < 1 && _period.Hours > 0)
+			{
+				Result = $"{_period.Hours} ساعت پیش";
+			}
+
+			return this;
+		}
+
+		public PeriodWrapper WithMinutes()
+		{
+			if (_period.Hours < 1 && _period.Minutes > 0)
+			{
+				Result = $"{_period.Minutes} دقیقه پیش";
+			}
+
+			return this;
+		}
+	}
+}

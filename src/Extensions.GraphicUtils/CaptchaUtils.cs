62459cb785715b6c61e2f16b2db5b9db73d52429
meisamhasani <meisam.hasani@gmail.com>
2019-03-06 14:20:53 +0330

    genrate captcha changed

--- src/Extensions.GraphicUtils/CaptchaUtils.cs
+++ src/Extensions.GraphicUtils/CaptchaUtils.cs
@@ -5,27 +5,53 @@ using SixLabors.ImageSharp.Processing;
 using SixLabors.Primitives;
 using SixLabors.Shapes;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Extensions.GraphicUtils
 {
     public class CaptchaUtils
     {
+        private static Random rng = new Random();
+
         public static string GenerateCaptchaBase64Image(string code)
         {
+            Guard.NotNullOrEmpty(code, nameof(code));
+
+            var positions = new List<byte> { 15, 7, 16, 23, 13 };
+            positions = ShuffleList<byte>(positions);
             FontCollection fonts = new FontCollection();
             FontFamily font = fonts.Install(@"arial.ttf");
-            var _font = font.CreateFont(7, FontStyle.Italic);
+            var _font = font.CreateFont(16, FontStyle.Italic);
             var stream = new MemoryStream();
             using (Image<Rgba32> img = new Image<Rgba32>(80, 40))
             {
-                using (var img2 = img.Clone(ctx => ctx.ApplyScalingWaterMark(_font, code, Rgba32.Black, 2, true)))
+                img.Mutate(x => x.DrawLines(Rgba32.Gray, 2, new PointF(0, 10), new PointF(80, 10)));
+                img.Mutate(x => x.DrawLines(Rgba32.Gray, 2, new PointF(0, 30), new PointF(80, 30)));
+                for (var i = 0; i < code.Length; ++i)
                 {
-                    img2.SaveAsPng(stream);
+                    img.Mutate(x => x.DrawText(code.Substring(i, 1), _font, Rgba32.Black, new PointF(i * 15 + 5, positions[i])));
                 }
+
+                img.SaveAsPng(stream);
             }
 
             return Convert.ToBase64String(stream.GetBuffer());
+        }
+
+        private static List<E> ShuffleList<E>(List<E> inputList)
+        {
+            List<E> randomList = new List<E>();
+            Random r = new Random();
+            int randomIndex = 0;
+            while (inputList.Count > 0)
+            {
+                randomIndex = r.Next(0, inputList.Count); //Choose a random object in the list
+                randomList.Add(inputList[randomIndex]); //add it to the new, random list
+                inputList.RemoveAt(randomIndex); //remove to avoid duplicates
+            }
+
+            return randomList; //return the new random list
         }
     }
 }

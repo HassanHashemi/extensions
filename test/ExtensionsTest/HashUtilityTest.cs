c2378148db32581fd34c8a28d9c58f4018c458a2
morteza.gheibi <morteza.gheibi@gmail.com>
2019-03-12 09:53:38 +0330

    remvoe #

--- test/ExtensionsTest/HashUtilityTest.cs
+++ test/ExtensionsTest/HashUtilityTest.cs
@@ -13,10 +13,11 @@ namespace ExtensionsTest
         [InlineData("در جریان #قتل #خاشقچی #دولت_عربستان  مقصر شناخته شده است! اما این #عرب ها زیر بار نمی روند البته این مهم نیست چون #وهابیت=انگلیسی هست!")]
         public void Should_Pass_TagExteraction(string data)
         {
+            
             var tagList = HashtagUtility.TagExteraction(data).ToList();
-            Assert.True(tagList[2] == "#دولت_عربستان");
-            Assert.False(tagList[3] == "#عرب ها");
-            Assert.True(tagList[4] == "#وهابیت");
+            Assert.True(tagList[2] == "دولت_عربستان");
+            Assert.False(tagList[3] == "عرب ها");
+            Assert.True(tagList[4] == "وهابیت");
         }
     }
 }

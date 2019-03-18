e62884d051b34973ac9ab12d5c33c6599c554ee3
Hassan Hashemi <HassanHashemi@yahoo.com>
2019-03-18 16:04:48 +0330

    add guid generator

---
+++ src/Extensions/GuidGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+
+namespace System
+{
+    /// <summary>
+    ///     Generates sequential <see cref="Guid" /> values using the same algorithm as NEWSEQUENTIALID()
+    ///     in Microsoft SQL Server. This is useful when entities are being saved to a database where sequential
+    ///     GUIDs will provide a performance benefit. The generated values are non-temporary, meaning they will
+    ///     be saved to the database.
+    /// </summary>
+    public sealed class SequentialGuidValueGenerator
+    {
+        private long _counter;
+
+        public SequentialGuidValueGenerator()
+        {
+            _counter = DateTime.UtcNow.Ticks;
+        }
+
+        /// <summary>
+        ///     Gets a value to be assigned to a property.
+        /// </summary>
+        /// <para>The change tracking entry of the entity for which the value is being generated.</para>
+        /// <returns> The value to be assigned to a property. </returns>
+        public Guid Next()
+        {
+            var guidBytes = Guid.NewGuid().ToByteArray();
+            var counterBytes = BitConverter.GetBytes(Interlocked.Increment(ref _counter));
+
+            if (!BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(counterBytes);
+            }
+
+            guidBytes[08] = counterBytes[1];
+            guidBytes[09] = counterBytes[0];
+            guidBytes[10] = counterBytes[7];
+            guidBytes[11] = counterBytes[6];
+            guidBytes[12] = counterBytes[5];
+            guidBytes[13] = counterBytes[4];
+            guidBytes[14] = counterBytes[3];
+            guidBytes[15] = counterBytes[2];
+
+            return new Guid(guidBytes);
+        }
+    }
+}
---
+++ test/ExtensionsTest/GuidGenereatorTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+
+namespace ExtensionsTest
+{
+    public class GuidGenereatorTest
+    {
+        [Fact]
+        public void ShouldGenerateGuid()
+        {
+            var generator1 = new SequentialGuidValueGenerator();
+            var generator2 = new SequentialGuidValueGenerator();
+            var first = generator1.Next();
+            var second = generator2.Next();
+            Assert.True(first != default(Guid));
+            Assert.True(second != default(Guid));
+            Assert.True(first != second);
+        }
+    }
+}

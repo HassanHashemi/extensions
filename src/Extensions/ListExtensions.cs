7ff8d36da168d9c21859e178f6e1bef89b7812f2
Hassan Hashemi <HassanHashemi@yahoo.com>
2019-06-12 01:40:19 -0700

    add swap method

---
+++ src/Extensions/ListExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace System
+{
+    public static class ListExtensions
+    {
+        public static void Swap<T>(this List<T> source, T first, T second)
+        {
+            Swap(source, source.IndexOf(first), source.IndexOf(second));
+        }
+
+        public static void Swap<T>(this List<T> source, int indexA, int indexB)
+        {
+            T tmp = source[indexA];
+            source[indexA] = source[indexB];
+            source[indexB] = tmp;
+        }
+    }
+}
---
+++ test/ExtensionsTest/ListTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ExtensionsTest
+{
+    public class ListTest
+    {
+        private class ListItem
+        {
+            public string Property { get; set; }
+        }
+
+        [Fact]
+        public void Should_Swap_items()
+        {
+            var first = new ListItem { Property = "val1" };
+            var second = new ListItem { Property = "val2" };
+            var third = new ListItem { Property = "val3" };
+            var fourth = new ListItem { Property = "val4" };
+
+            var list = new List<ListItem>() { first, second, third, fourth };
+
+            list.Swap(first, third);
+
+            Assert.True(list[0].Property == "val3");
+            Assert.True(list[2].Property == "val1");
+        }
+    }
+}

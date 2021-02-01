8962d8dfc62538ce84bb20a9510aa6a8778d57dd
Hassan Hashemi <HassanHashemi@yahoo.com>
2021-01-31 22:20:45 -0800

    inc version

---
+++ src/Extensions.Http/Mvc/RequireValidModel.cs
@@ -0,0 +1,28 @@
+﻿using Extensions.Http.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Linq;
+using System.Net;
+
+namespace Portal.Web.Infra.Filters
+{
+    public class RequireValidModel : Attribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ModelState.ValidationState != ModelValidationState.Valid)
+            {
+                var data = context.ModelState
+                    .Where(i => i.Value.ValidationState != ModelValidationState.Valid)
+                    .Select(ApiErrorEntry.Format);
+
+                context.Result = new ApiResult(HttpStatusCode.BadRequest, data);
+            }
+        }
+    }
+}

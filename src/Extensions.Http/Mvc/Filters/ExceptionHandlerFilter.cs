16747ca79cd24ffaf78dc73f24b79a4cf2acf345
Hassan Hashemi <HassanHashemi@yahoo.com>
2021-05-27 11:00:00 +0430

    ic versioN

---
+++ src/Extensions.Http/Mvc/Filters/ExceptionHandlerFilter.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Net;
+
+namespace Extensions.Http.Mvc.Filters
+{
+    public class ExceptionHandlerFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var exception = context.Exception;
+
+            if (exception is EntityNotFoundException notFound)
+            {
+                context.Result = new ApiResult(HttpStatusCode.NotFound, notFound.Message);
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}

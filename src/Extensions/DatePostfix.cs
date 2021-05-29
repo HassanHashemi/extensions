d222647e6699df3186ad9f7920395e448dfd6bca
Hassan Hashemi <HassanHashemi@yahoo.com>
2021-05-29 13:09:17 +0430

    add helpers

---
+++ src/Extensions/DatePostfix.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace Extensions
+{
+    public enum DatePostfix
+    {
+        [Description("ثانیه")]
+        Seconds,
+
+        [Description("دقیقه")]
+        Minute,
+
+        [Description("ساعت")]
+        Hours,
+
+        [Description("روز")]
+        Days,
+
+        [Description("هفته")]
+        Week,
+
+        [Description("ماه")]
+        Month,
+
+        [Description("سال")]
+        Year
+    }
+}
---
+++ src/Extensions/PersianDateWrapper.cs
@@ -0,0 +1,14 @@
+﻿namespace Extensions
+{
+    public class PersianDateWrapper
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public int? Hours { get; set; }
+        public int? Minutes { get; set; }
+        public int? Seconds { get; set; }
+
+        public string Full => $"{this.Year}/{this.Month}/{this.Day}";
+    }
+}
